6bdef0cbac8c2353248c9fd17fc035c93b752edd
Lonnie Arellano <lonnie.j.arellano@gmail.com>
2022-11-30 16:58:18 -0600

    created isEven Function

--- tests.js
+++ tests.js
@@ -60,8 +60,45 @@ describe('isFive', function () {
 
 });
 
+describe('isEven', function() {
+    it('should be a defined function', function() {
+        expect(typeof isEven).toBe('function')
+    });
+    it('should return a boolean', function() {
+        expect(typeof isEven()).toBe('boolean')
+    });
+    it('should return true when given 2', function() {
+        expect(isEven(2)).toBe(true)
+    });
+    it('should return true when given -4', function() {
+        expect(isEven(-4)).toBe(true)
+    });
+    it('should return false when given 3', function() {
+        expect(isEven(3)).toBe(false)
+    });
+    it('should return true when given 8', function() {
+        expect(isEven(8)).toBe(true)
+    });
+    it('should return false when given Infinity', function() {
+        expect(isEven(Infinity)).toBe(false)
+    });
+    it('should return false when given banana', function() {
+        expect(isEven('banana')).toBe(false)
+    });
+    it('should return false when given a boolean', function() {
+        expect(typeof isEven == Boolean).toBe(false)
+    });
+    // it('should return false when not given a argument', function() {
+    //     expect (isEven("")).toBe(false)
+    // });
+})
 
 
+
+
+
+
+// returns false when called without an argument like isEven()
 
 
 
